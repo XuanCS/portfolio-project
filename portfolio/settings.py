fecbfbb3a7dccd90ecd03e5d3d225d128f7218ee
Xuan Lu <xuanlu@iastate.edu>
2018-07-14 15:52:24 -0400

    change pwd

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': '45lvxuan88',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
